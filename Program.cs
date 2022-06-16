e5d610624566d53d3efddf1ec86675def07ba075
Berke Akgün <berkeakgun9@gmail.com>
2022-06-16 12:10:39 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+Console.WriteLine("Welcome to the calculate circle area app");
+
+Console.WriteLine("Enter a radius");
+var value = Console.ReadLine();
+
+if (string.IsNullOrEmpty(value))
+{
+    Console.WriteLine("Hata");
+    return;
+}
+if(int.TryParse(value,out int x)){
+
+}
+else
+{
+    Console.WriteLine("Hata");
+    return;
+}
+
+
+int radius = Convert.ToInt32(value);
+double area_of_circle = (Math.PI * radius * radius);
+
+double circum_of_circle = (2 * Math.PI * radius);
+
+Console.WriteLine("The area of circle = " + area_of_circle);
+Console.WriteLine("The circum of circle = " + circum_of_circle);
